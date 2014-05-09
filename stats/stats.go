db2f1b48c8b1e8301c594d3e8f0a6333ac7c571a
Daniel Martí <mvdan@mvdan.cc>
2014-05-10 01:24:10 +0200

    Rewritten to use microdescriptor consensuses

--- stats/stats.go
+++ stats/stats.go
@@ -3,6 +3,7 @@ package main
 import (
 	"archive/tar"
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -37,30 +38,20 @@ func (b ByteSize) String() string {
 }
 
 type Consensus struct {
-	Time time.Time
-	Ids  []string
-	Size int64
-}
-
-type Diff struct {
-	Added   int
-	Removed int
+	Time   time.Time
+	Ids    []string
+	Hashes []string
+	Size   int64
 }
 
 type Consensuses []Consensus
 
-type IntervalStats struct {
-	Diffs        []Diff
-	MeanDiffSize ByteSize
-}
-
 type ConsensusStats struct {
-	Data              Consensuses
-	IS                []IntervalStats
-	MeanEntrySize     ByteSize
-	MeanConsensusSize ByteSize
-	TotalSize         int64
-	TotalEntryCount   int
+	Data            Consensuses
+	MeanEntrySize   ByteSize
+	MeanMicroSize   ByteSize
+	TotalSize       int64
+	TotalEntryCount int
 }
 
 func (c Consensuses) Len() int {
@@ -80,7 +71,8 @@ const (
 	dateOutForm = "2006-01-02-15-04-05"
 )
 
-func analyze(cs Consensuses, tr *tar.Reader) Consensuses {
+func analyze(cs Consensuses, tr *tar.Reader) (Consensuses, ByteSize) {
+	var totalMicroSize, totalMicroCount int64 = 0, 0
 	for {
 		hdr, err := tr.Next()
 		if err == io.EOF {
@@ -90,7 +82,13 @@ func analyze(cs Consensuses, tr *tar.Reader) Consensuses {
 			log.Fatalln(err)
 		}
 		if hdr.Typeflag != tar.TypeReg {
-			log.Println(hdr.Name)
+			//log.Println(hdr.Name)
+			continue
+		}
+
+		if strings.Contains(hdr.Name, "/micro/") {
+			totalMicroSize += hdr.Size
+			totalMicroCount++
 			continue
 		}
 
@@ -100,13 +98,22 @@ func analyze(cs Consensuses, tr *tar.Reader) Consensuses {
 		scanner := bufio.NewScanner(tr)
 		for scanner.Scan() {
 			text := scanner.Text()
-			switch text[0] {
-			case 'r':
+			switch text[0:2] {
+			case "r ":
 				fields := strings.Split(text, " ")
 				if len(fields) < 3 {
-					continue
+					log.Fatal("Missing identity: " + text)
 				}
 				c.Ids = append(c.Ids, fields[2])
+			case "m ":
+				fields := strings.Split(text, " ")
+				if len(fields) < 2 {
+					log.Fatal("Missing hash: " + text)
+				}
+				c.Hashes = append(c.Hashes, fields[1])
+				if len(c.Ids) != len(c.Hashes) {
+					log.Fatal("The number of identities and hashes differ!")
+				}
 			default:
 				continue
 			}
@@ -116,72 +123,98 @@ func analyze(cs Consensuses, tr *tar.Reader) Consensuses {
 		}
 		cs = append(cs, c)
 	}
-	return cs
+	meanMicroSize := ByteSize(totalMicroSize / totalMicroCount)
+	return cs, meanMicroSize
 }
 
-func results(cst ConsensusStats, maxInterval int) {
-	cs := cst.Data
-	if len(cs) == 0 {
+func prepare(cst *ConsensusStats) {
+	if len(cst.Data) == 0 {
+		fmt.Println("No data to show.")
 		return
 	}
-	sort.Sort(cs)
-	cst.TotalSize = 0
-	for _, c := range cs {
+	sort.Sort(cst.Data)
+	fmt.Printf("== Global data ==\n")
+
+	for _, c := range cst.Data {
 		cst.TotalSize += c.Size
 		cst.TotalEntryCount += len(c.Ids)
 	}
-	cst.MeanConsensusSize = ByteSize(cst.TotalSize) / ByteSize(len(cs))
+
 	cst.MeanEntrySize = ByteSize(cst.TotalSize) / ByteSize(cst.TotalEntryCount)
-	fmt.Printf("== Global data ==\n")
+	fmt.Printf("Note that ALL of the sizes shown are in uncompressed bytes\n")
 	fmt.Printf("Number of consensuses: %d\n", len(cst.Data))
-	fmt.Printf("Mean consensus size: %s\n", cst.MeanConsensusSize.String())
-	fmt.Printf("Mean entry size: %s\n", cst.MeanEntrySize.String())
-	cst.IS = make([]IntervalStats, maxInterval)
-	for interval := 1; interval <= maxInterval; interval++ {
-		intervalStats := cst.IS[interval-1]
-		diffs := intervalStats.Diffs
-		var totalDiffEntryCount int = 0
-		for i, c := range cs {
-			diff := Diff{}
-			var prevIds []string
-			prev := i - interval
-			if prev >= 0 {
-				prevIds = cs[prev].Ids
-			} else {
-				prevIds = make([]string, 0)
+	fmt.Printf("Mean consensus entry size: %s\n", cst.MeanEntrySize.String())
+	fmt.Printf("Mean microdescriptor size: %s\n", cst.MeanMicroSize.String())
+}
+
+func results(cst ConsensusStats, maxInterval int, keep int) {
+	fmt.Printf("When keeping non-running relays for %d hours...\n", keep)
+	var totalConsensusEntries int64 = 0
+	cs := make([]map[string]bool, len(cst.Data))
+	hs := make([]map[string]bool, len(cst.Data))
+	for i := range cst.Data {
+		c := make(map[string]bool)
+		h := make(map[string]bool)
+		for k := 0; k <= keep; k++ {
+			if k > i { continue }
+			for _, id := range cst.Data[i-k].Ids {
+				c[id] = true
 			}
-			prevIdsMap := make(map[string]bool)
-			for _, id := range prevIds {
-				prevIdsMap[id] = true
+			for _, hash := range cst.Data[i-k].Hashes {
+				h[hash] = true
 			}
-			for _, id := range c.Ids {
-				a, e := prevIdsMap[id]
-				if e && a {
-				} else {
-					diff.Added++
-				}
-				delete(prevIdsMap, id)
-			}
-			diff.Removed += len(prevIdsMap)
-			// Plus one to simulate overhead
-			totalDiffEntryCount += diff.Added + diff.Removed + 1
-			diffs = append(diffs, diff)
 		}
-		intervalStats.MeanDiffSize = ByteSize(totalDiffEntryCount) * (cst.MeanEntrySize / ByteSize(len(diffs)))
-		fmt.Printf("== Interval %dh ==\n", interval)
-		fmt.Printf("Mean diff size is %s\n", intervalStats.MeanDiffSize.String())
+		totalConsensusEntries += int64(len(c))
+		cs[i], hs[i] = c, h
+	}
+
+	meanConsensusSize := ByteSize(totalConsensusEntries) * (cst.MeanEntrySize / ByteSize(len(cs)))
+	fmt.Printf("Mean consensus size: %s\n", meanConsensusSize.String())
+
+	for interval := 1; interval <= maxInterval; interval++ {
+		var totalDiffSize, totalMicroSize ByteSize = 0, 0
+		for i := range cs {
+			if interval > i { continue }
+			oldIds := cs[i-interval]
+			newIds := cs[i]
+			oldHashes := hs[i-interval]
+			newHashes := hs[i]
+			for hash := range newHashes {
+				_, e := oldHashes[hash]
+				if !e {
+					totalMicroSize += cst.MeanMicroSize
+				}
+			}
+			for id := range newIds {
+				_, e := oldIds[id]
+				// Added
+				if !e {
+					totalDiffSize += cst.MeanEntrySize + ByteSize(3.0)
+				}
+			}
+			for id := range oldIds {
+				_, e := newIds[id]
+				// Deleted
+				if !e {
+					totalDiffSize += ByteSize(6.0)
+				}
+			}
+		}
+		meanDiffSize := totalDiffSize / ByteSize(len(cs))
+		fmt.Printf("Mean consensus diff size when interval is %dh: %s\n",
+			interval, meanDiffSize.String())
+		meanMicroSize := totalMicroSize / ByteSize(len(cs))
+		fmt.Printf("Mean microdescriptor download size when interval is %dh: %s\n",
+			interval, meanMicroSize.String())
 	}
 }
 
 func main() {
-	tarpaths, err := filepath.Glob("consensuses-*.tar.*")
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	cst := ConsensusStats{}
+	flag.Parse()
 
-	for _, tp := range tarpaths {
+	for _, tp := range flag.Args() {
 		fmt.Printf("Parsing %s\n", tp)
 		ext := filepath.Ext(tp)
 		var catcmd string
@@ -207,7 +240,10 @@ func main() {
 			log.Fatal(err)
 		}
 		tr := tar.NewReader(fr)
-		cst.Data = analyze(cst.Data, tr)
+		cst.Data, cst.MeanMicroSize = analyze(cst.Data, tr)
 	}
-	results(cst, 10)
+	prepare(&cst)
+	for keep := 0; keep <= 12; keep++ {
+		results(cst, 12, keep)
+	}
 }
