acb880778cf8944bb19d3e460651433e0ab137ab
Daniel Martí <mvdan@mvdan.cc>
2014-06-21 20:39:20 +0200

    Move struct into header, simpler smartlist frees

--- consdiff.h
+++ consdiff.h
@@ -1,2 +1,12 @@
+/** Data structure to define a slice of a smarltist. */
+typedef struct {
+  /** Smartlist that this slice is made from. */
+  smartlist_t *list;
+  /** Starting position of the smartlist. */
+  int offset;
+  /** Number of elements in the slice. */
+  int len;
+} smartlist_slice_t;
+
 smartlist_t *gen_diff(smartlist_t *cons1, smartlist_t *cons2);
 smartlist_t *apply_diff(smartlist_t *cons1, smartlist_t *diff);
