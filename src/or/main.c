faf934a54aeea7e3599c38a850ca0c2924458c75
Daniel Martí <mvdan@mvdan.cc>
2014-08-06 11:26:51 +0200

    Initial support for consensus diff handling

--- src/or/main.c
+++ src/or/main.c
@@ -1965,6 +1965,12 @@ do_main_loop(void)
     log_warn(LD_DIR,
              "Couldn't load all cached v3 certificates. Starting anyway.");
   }
+  const or_options_t *options = get_options();
+  /* See what consensuses have we cached on disk. */
+  if (directory_caches_dir_info(options)) {
+    dirserv_refresh_stored_consensuses();
+    dirserv_remove_old_consensuses();
+  }
   if (router_reload_consensus_networkstatus()) {
     return -1;
   }
