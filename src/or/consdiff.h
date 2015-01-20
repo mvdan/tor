8e672af2156560d153a05f2285492e3669fd4603
Daniel Martí <mvdan@mvdan.cc>
2015-01-20 18:35:04 -0500

    consdiff: don't expose smartlist_slice_t
    It is not (yet?) used anywhere else in the codebase

--- src/or/consdiff.h
+++ src/or/consdiff.h
@@ -7,19 +7,6 @@
 
 #include "or.h"
 
-/** Data structure to define a slice of a smarltist. */
-typedef struct {
-  /**
-   * Smartlist that this slice is made from.
-   * References the whole original smartlist that the slice was made out of.
-   * */
-  smartlist_t *list;
-  /** Starting position of the slice in the smartlist. */
-  int offset;
-  /** Length of the slice, i.e. the number of elements it holds. */
-  int len;
-} smartlist_slice_t;
-
 smartlist_t *
 consdiff_gen_diff(smartlist_t *cons1, smartlist_t *cons2,
                   digests_t *digests1, digests_t *digests2);
